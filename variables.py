f22079b2375607d5df04586a2460217b1e58e758
Kacper <fr0stY7@users.noreply.github.com>
2025-10-30 01:15:36 +0100

    Getting know about variables

---
+++ variables.py
@@ -0,0 +1,13 @@
+# Variables
+
+#string
+
+name = "Thomas";
+second_name = "Marcus";
+print(f"Your name is {name} and your second name is {second_name}");
+print("Nice to meet you!");
+
+#integer
+
+age = 25;
+print(f"You have {age} years old");
