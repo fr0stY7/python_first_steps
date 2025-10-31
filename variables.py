980e45cd7bcca6a9d5adb7ee4d55ac42ec8519f8
Kacper <fr0stY7@users.noreply.github.com>
2025-11-01 00:46:55 +0100

    Just add float and boolean

--- variables.py
+++ variables.py
@@ -10,4 +10,23 @@ print("Nice to meet you!");
 #integer
 
 age = 25;
+numbers_of_goals = 3;
 print(f"You have {age} years old");
+print(f"In this match team scored {numbers_of_goals}");
+
+#float
+
+price = 3.99;
+
+print(f"Price for this juice is {price}");
+
+#boolean
+
+is_true = True;
+
+if is_true:
+    print("That's true!")
+else:
+    print("That's not true")
+
+
