42e663857ff08e6173962b7eab0f542c0e599944
Kacper <fr0stY7@users.noreply.github.com>
2025-11-02 00:17:58 +0100

    Do a simple age checker using input and if

---
+++ age_checker.py
@@ -0,0 +1,11 @@
+your_name = input("Please, insert your name:");
+your_age = int(input("Please, insert your age:"));
+
+print(f"Hello {your_name}!");
+print(f"You have {your_age} years old!");
+
+if your_age >= 18:
+    print("You are an adult")
+else:
+    print("You are not an adult ")
+    
